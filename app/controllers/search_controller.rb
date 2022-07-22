53fba7836dc3fe38fa2ac5f7cf62bfe42d35ef6e
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-22 10:22:50 -0600

    Update Search Controller Index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-
+    @nation = params[:nation]
+    @all_characters = TheLastAirbenderFacade.create_character_data_from_nation(@nation)
+    @twenty_five_characters = @all_characters[0..24]
   end
-end    
+end

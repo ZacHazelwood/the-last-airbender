471f86761af411f05de7469bb2d10bcfe067b2a1
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-22 10:54:33 -0600

    Feat: Search Index displays correct content

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     @nation = params[:nation]
-    @all_characters = TheLastAirbenderFacade.create_character_data_from_nation(@nation)
+    @all_characters = TheLastAirbenderFacade.create_character_data_from_nation(@nation.gsub("_", "+").titleize)
     @twenty_five_characters = @all_characters[0..24]
   end
 end

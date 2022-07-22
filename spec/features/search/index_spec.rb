b1481ef399b4ce156db2999c66bca0085448457b
Zachary Hazelwood <zhaze001@gmail.com>
2022-07-22 10:30:19 -0600

    Format Index display tests

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe 'Search Index Page' do
 
     expect(current_path).to eq("/search")
   end
+
+  it "displays information" do
+    visit "/"
+
+    select("Air Nomads")
+    click_button("Search For Members")
+
+    expect(page).to have_content("Air Nomads")
+  end
 end
